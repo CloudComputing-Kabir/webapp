3116b41891d19a2d8086020d1f026d773d4f1f51
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-02-23 00:01:36 -0500

    Packer for cloud assignment 4

---
+++ app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "---------------current directory - /home/ubuntu----------------"
+# get current directory //home/ubuntu
+pwd
+# get into webapp
+cd ./webapp || exit
+
+echo "---------------installing app dependencies - NPM----------------"
+# install all the dependencies
+npm install
+npm ci
+
+echo "---------------hosting up the app - PM2 config----------------"
+# start app using pm2
+sudo pm2 start npm --name "csye6225-webapp" -- run "start"
+sudo pm2 startup systemd
+# pm2 save the list
+sudo pm2 save
+# pm2 list all the running apps
+sudo pm2 list
---
+++ artifacts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pwd
+rm -rf node_modules
+rm -rf webapp.zip
+zip -r webapp.zip  ./
